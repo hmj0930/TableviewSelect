6d571ac598bb1112ad7c3263a8ae6d667ead4799
HanMJ <915660592@qq.com>
2017-11-29 08:31:27 +0800

    Initial Commit

---
+++ TableviewSelect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableviewSelect
+//
+//  Created by MJ on 2017/11/29.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
